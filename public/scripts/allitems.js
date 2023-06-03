e5e742f124482975f7d99e9e762a83f5fd4861f1
Ronald Kelechi <96204229+ronaldkelechi11@users.noreply.github.com>
2023-06-02 22:58:32 -0700

    Done with All Items REM(fetching)

--- public/scripts/allitems.js
+++ public/scripts/allitems.js
@@ -1,4 +1,13 @@
 function run() {
-
+    console.log("Item clicked");
+    var item = document.querySelectorAll(".item")
+    for (let i = 0; i < item.length; i++) {
+        console.log(item.length);
+        item[i].addEventListener("click", () => {
+            var itemName = item[i].querySelector(".title").innerHTML
+            console.log(itemName);
+            window.location = "/productview/" + itemName
+        })
+    }
 }
 run()
