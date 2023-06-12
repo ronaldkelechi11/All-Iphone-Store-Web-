fcb639477df1892d3d66d4b535975173c9f2cecc
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-06-12 03:02:34 -0700

    minor update

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ app.listen(PORT, () => {
     mongoose.connect(connectionUrl)
         .then(() => { console.log("Connection to Mongo DB Secured"); })
         .catch((error) => {
-            console.log("Error");
+            console.log(error);
         })
 })
